526b3ef435989a391b3525c59f7b819d27e4df34
Débora Sete Acosta <sete.debora@gmail.com>
2025-03-26 23:06:27 -0300

    first commit

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+
+  // {
+    // Regras personalizadas
+  //   rules: {
+  //     "indent": ["error", 2],
+  //     "no-trailing-spaces": "error",
+  //     "linebreak-style": ["error", "unix"],
+  //     "quotes": ["error", "single"],
+  //   },
+  // },
+]);
