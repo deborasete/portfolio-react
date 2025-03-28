72c6658a4ab5911762edd0d4172a65d0d4090c86
Débora Sete Acosta <sete.debora@gmail.com>
2025-03-28 16:52:40 -0300

    styling the sidebar

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,5 @@
+import { FotoPerfil } from './styles'
+
+const Avatar = () => <FotoPerfil src="https://github.com/deborasete.png" />
+
+export default Avatar
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,21 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+import { BotaoTema, Descricao, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/deborasete.png" />
-    <Titulo fontSize={20}>Débora Sete</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Débora Sete</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        deborasete
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Front-end Developer
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
